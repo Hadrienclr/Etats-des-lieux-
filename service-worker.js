7ae8b6c1aac03b8be641ecfd73b34fe233082349
Hadrienclr <z5vsggxzm5@privaterelay.appleid.com>
2026-07-15 16:32:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="constat-v2.0.0";
-const ASSETS=["./","./index.html","./logic-v2.js","./manifest.webmanifest","./logo.svg","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./favicon-32.png"];
+const CACHE="edl-v3.0.0";
+const ASSETS=["./","./index.html","./logic-v2.js","./mobile-v3.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./favicon-32.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
